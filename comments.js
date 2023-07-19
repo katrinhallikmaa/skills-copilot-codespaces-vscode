c3d00ccb7d8c485b4fb583d46b626219432365fe
katrinhallikmaa <60192901+katrinhallikmaa@users.noreply.github.com>
2023-07-19 13:33:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,81 +1,62 @@
-// create web server with express
-// npm install express
-// npm install body-parser
-// npm install mysql
-// npm install express-session
-// npm install multer
-// npm install fs
-// npm install ejs
-// npm install express-ejs-layouts
-// npm install passport
-// npm install passport-local
-// npm install passport-facebook
-// npm install passport-twitter
-// npm install passport-kakao
-// npm install passport-naver
-// npm install connect-flash
-// npm install bcrypt-nodejs
-// npm install cookie-parser
-// npm install nodemailer
-// npm install nodemailer-smtp-transport
-// npm install socket.io
-// npm install multer
-// npm install fs
-// npm install ejs
-// npm install express-ejs-layouts
-// npm install passport
-// npm install passport-local
-// npm install passport-facebook
-// npm install passport-twitter
-// npm install passport-kakao
-// npm install passport-naver
-// npm install connect-flash
-// npm install bcrypt-nodejs
-// npm install cookie-parser
-// npm install nodemailer
-// npm install nodemailer-smtp-transport
-// npm install socket.io
-// npm install multer
-// npm install fs
-// npm install ejs
-// npm install express-ejs-layouts
-// npm install passport
-// npm install passport-local
-// npm install passport-facebook
-// npm install passport-twitter
-// npm install passport-kakao
-// npm install passport-naver
-// npm install connect-flash
-// npm install bcrypt-nodejs
-// npm install cookie-parser
-// npm install nodemailer
-// npm install nodemailer-smtp-transport
-// npm install socket.io
-// npm install multer
-// npm install fs
-// npm install ejs
-// npm install express-ejs-layouts
-// npm install passport
-// npm install passport-local
-// npm install passport-facebook
-// npm install passport-twitter
-// npm install passport-kakao
-// npm install passport-naver
-// npm install connect-flash
-// npm install bcrypt-nodejs
-// npm install cookie-parser
-// npm install nodemailer
-// npm install nodemailer-smtp-transport
-// npm install socket.io
-// npm install multer
-// npm install fs
-// npm install ejs
-// npm install express-ejs-layouts
-// npm install passport
-// npm install passport-local
-// npm install passport-facebook
-// npm install passport-twitter
-// npm install passport-kakao
-// npm install passport-nav
+// Create web server
 
+// Import statements
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 
+// Create web server
+const app = express();
+
+// Set port
+const port = 3000;
+
+// Set directory for static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set directory for views
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Set body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Import comments
+const comments = require('./comments.json');
+
+// GET /comments
+// Display comments
+app.get('/comments', (req, res) => {
+    res.render('comments', {
+        title: 'Comments',
+        comments: comments
+    });
+});
+
+// POST /comments
+// Add new comment
+app.post('/comments', (req, res) => {
+    // Get comment from request body
+    const comment = req.body;
+    // Add comment to comments array
+    comments.push(comment);
+    // Write comments to comments.json
+    fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Comments saved to comments.json');
+        }
+    });
+    // Redirect to /comments
+    res.redirect('/comments');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
