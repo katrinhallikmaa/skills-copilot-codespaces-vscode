9b310477406f209da04693d3985759949f40c370
katrinhallikmaa <60192901+katrinhallikmaa@users.noreply.github.com>
2023-07-19 13:08:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,35 @@
 // create web server with express
-const express = require("express");
-const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const { Comment } = require("../models");
-const { asyncHandler } = require("../utils");
+const express = require('express');
+const app = express();
+// create web server with express
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const commentsPath = path.join(__dirname, '../data/comments.json');
+// const comments = require('../data/comments.json');
+const { v4: uuidv4 } = require('uuid');
+// const { getComments, getComment, saveComment, updateComment, deleteComment } = require('../controllers/commentsController');
+const { getComments, getComment, saveComment, updateComment, deleteComment } = require('../controllers/commentsController');
+
+// middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// GET /comments
+app.get('/', getComments);
 
 // GET /comments/:id
-// return all comments for a given book
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const comments = await Comment.findAll({
-      where: { bookId: req.params.id },
-    });
-    res.json(comments);
-  })
-);
+app.get('/:id', getComment);
 
 // POST /comments
-// create a new comment
-router.post(
-  "/",
-  [
-    check("comment").exists().withMessage("Please provide a comment"),
-    check("bookId").exists().withMessage("Please provide a bookId"),
-  ],
-  asyncHandler(async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map((error) => error.msg);
-      res.status(400).json({ errors: errorMessages });
-    } else {
-      const comment = await Comment.create(req.body);
-      res.status(201).json(comment);
-    }
-  })
-);
+app.post('/', saveComment);
 
-module.exports = router;
+// PUT /comments/:id
+app.put('/:id', updateComment);
+
+// DELETE /comments/:id
+app.delete('/:id', deleteComment);
+
+module.exports = app;
